a2d5a6984a9e51ba840968d288c4e9fc0d548623
Steffen Hvid Hansen <shh@commentor.dk>
2024-04-30 07:54:47 +0200

    Added changes

--- runner.py
+++ runner.py
@@ -1,22 +1,32 @@
-import math
-from itertools import product
+import numpy as np
 
-def count_pal(n):
-    accounting = {}
-    if n > 0:
-        accounting[1] = [9, 9]
-    # if n > 1:
-    #     accounting[2] = [9, 9 + accounting[1][1]]
-    # if n > 2:
-    for i in range(2,n+1):
-        alpha = math.floor(i/2)
-        if i % 2 == 0:
-            amount = int("1"+"0" * (alpha-1)) * 9
-            accounting[i] = [amount, amount + accounting[i-1][1]] 
-        else:
-            amount = int("1"+"0"*alpha) * 9
-            accounting[i] = [amount, amount + accounting[i-1][1]]
+test = [[97, -44, -47, -10129], [23, 64, 17, 119], [-30, -6, 82, 2244]]
+
+def gauss_seidel(coeff_matrix):
+    x,y,z = 0,0,0
     
-    return accounting.get(n, None)
+    xe = coeff_matrix[0]
+    ye = coeff_matrix[1]
+    ze = coeff_matrix[2]
 
-print(count_pal(3))
+
+    for i in range(100):
+        xc = 1/xe[0] * (y * -1 * xe[1] + z * -1 * xe[2] + xe[3])
+        yc = 1/ye[1] * (x * -1 * ye[0] + z * -1 * ye[2] + ye[3])
+        zc = 1/ze[2] * (x * -1 * ze[0] + y * -1 * ze[1] + ze[3])
+
+        if ct(xc,x) and ct(yc,y) and ct(zc,z):
+            return ([xc,yc,zc], i+1-5)
+        else:
+            x = xc
+            y = yc
+            z = zc
+    return (x,y,z)
+
+def ct(a,b):
+    return np.abs(a-b) <= 0.0001
+
+print(gauss_seidel(test))
+
+# ([3.0167567484114746, 1.9858893605869723, 0.9118159523141861], 7)))
+# (3.0167548500881836, 1.9858906525573194, 0.9118165784832449)
